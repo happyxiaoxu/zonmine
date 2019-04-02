3efc5e576eb09738c2c205358402a08b9796574f
happyxiaoxu <happyxiaoxu@outlook.com>
2019-04-02 10:33:22 -0800

    deploying middle

--- Azondrop/wsgi.py
+++ Azondrop/wsgi.py
@@ -12,6 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Azondrop.settings")
-
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
 
